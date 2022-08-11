59a72a8031cceb7c471bff9ced3c828435050527
Hridoy Das <>
2022-08-12 00:08:56 +0900

    another way to single and multiple permission in accopanist libraray

--- app/src/main/java/com/example/jetpackcomposepractice/permissionCheck/PermissionStateExt.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/permissionCheck/PermissionStateExt.kt
@@ -1,9 +1,6 @@
 package com.example.jetpackcomposepractice.permissionCheck
 
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-
-@OptIn(ExperimentalPermissionsApi::class)
-fun PermissionState.isPermanentDenied(): Boolean {
-    return !shouldShowRationale && !hasPermission
-}
+//@OptIn(ExperimentalPermissionsApi::class)
+//fun PermissionState.isPermanentDenied(): Boolean {
+//    return !shouldShowRationale && !hasPermission
+//}
